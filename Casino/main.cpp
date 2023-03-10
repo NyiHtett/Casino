4144063a6746306c15ae9f707e6d9c0319e5c8dc
Nyi Htet <nyihtet4dev@icloud.com>
2023-03-10 00:39:24 -0800

    first commit

--- Casino/main.cpp
+++ Casino/main.cpp
@@ -1,14 +1,28 @@
-//
-//  main.cpp
-//  Casino
-//
-//  Created by Nyi Htet on 3/10/23.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <string>
+#include <vector>
+using namespace std;
+int main(){
+    string message;
+    vector<string> deck;
+    string First[] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    char Shape[] = {'S','H','C','D'};
+    for(int i = 0; i < 13; i++){
+        for(int j = 0; j < 4; j++){
+            deck.push_back(First[i]+Shape[j]);
+        }
+    }
+    do{
+        srand(static_cast<int>(time(0)));
+        int r = rand() % deck.size();         // get random number
+        cout << "Card: " << deck[r] << endl;  // print out the card
+        deck.erase(deck.begin() + r);
+        if (deck.size() == 0){
+            break;
+        }
+        cin >> message;
+    }while(message != "quit");
 }
